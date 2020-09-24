1efb9bd52bffc8314f2fb1785f6baba39535e6ce
georgysemenov <71846417+georgysemenov@users.noreply.github.com>
2020-09-24 12:51:06 -0600

    Update Comparison of inheritance models.R

--- code/Comparison of inheritance models.R
+++ code/Comparison of inheritance models.R
@@ -1,8 +1,11 @@
 library(tidyverse)
 data <- read.table("compound.genotypes1A.20.txt", header=T)
 #this dataset includes only individuals sampled within alba and personata hybrid zone
+# head.plumage.value is a measure of the amount of white coloration on the head and neck plumage (see Semenov et al. 2017 Mol. Ecol. for plumage quantification details)
+# Input file contains genotypic information for two genomic regions on chromsome 1A (aa=alba allele, aP=heterozygote, PP=personata allele) and 20 (AA=alba allele, Ap=heterozygote,
+# pp=personata alleles) encoded as biallelic loci. Alleles with partially dominant inheritance are capitilized.
 
-# Make a dataset with addiditive effects in both 1A and 20
+# Model with addiditive effects in both 1A and 20
 data$add1A <- gsub("PP", 1, data$genotype.1A)
 data$add1A <- gsub("aP", 0.5, data$add1A)
 data$add1A <- gsub("aa", 0, data$add1A)
@@ -15,9 +18,9 @@ data$add20 <- as.numeric(data$add20)
 
 additive <- lm(head.plumage.value ~ add1A + add20, data = data)
 
-# Make a partially dominant model where 
-# 1A is partially dominant in personata (heterozygote is 25% more similar to personata) and
-# 20 is partially dominant in alba (heterozygote is 25% more similar to alba)
+# Partially dominant model where:
+# 1A is partially dominant in personata (heterozygote is 25% more similar to personata compared to addive model) and
+# 20 is partially dominant in alba (heterozygote is 25% more similar to alba compared to addive model)
 data$pers.dom.1A <- gsub("PP", 1, data$genotype.1A)
 data$pers.dom.1A <- gsub("aP", 0.75, data$pers.dom.1A)
 data$pers.dom.1A <- gsub("aa", 0, data$pers.dom.1A)
@@ -31,8 +34,8 @@ data$alba.dom20 <- as.numeric(data$alba.dom20)
 pers.dom.1A_alba.dom20 <- lm(head.plumage.value ~ pers.dom.1A + alba.dom20, data = data)
 
 # Make a partially dominant model where 
-# 1A is partially dominant in alba (heterozygote is 25% more similar to alba) and
-# 20 is partially dominant in personata (heterozygote is 25% more similar to personata)
+# 1A is partially dominant in alba (heterozygote is 25% more similar to alba compared to addive model) and
+# 20 is partially dominant in personata (heterozygote is 25% more similar to personata compared to addive model)
 data$alba.dom1A <- gsub("PP", 0, data$genotype.1A)
 data$alba.dom1A <- gsub("aP", 0.75, data$alba.dom1A)
 data$alba.dom1A <- gsub("aa", 1, data$alba.dom1A)
@@ -46,14 +49,14 @@ data$pers.dom20 <- as.numeric(data$pers.dom20)
 alba.dom.1A_pers.dom20 <- lm(head.plumage.value ~ alba.dom1A + pers.dom20 , data = data)
 
 # Make a partially dominant model where 
-# 1A is partially dominant in alba (heterozygote is 25% more similar to alba)
-# 20 is partially dominant in alba (heterozygote is 25% more similar to alba)
+# 1A is partially dominant in alba (heterozygote is 25% more similar to alba compared to addive model)
+# 20 is partially dominant in alba (heterozygote is 25% more similar to alba compared to addive model)
 
 alba.dom.1A_alba.dom20 <- lm(head.plumage.value ~ alba.dom1A + alba.dom20 , data = data)
 
 # Make a partially dominant model where 
-# 1A is partially dominant in personata (heterozygote is 25% more similar to personata)
-# 20 is partially dominant in personata (heterozygote is 25% more similar to personata)
+# 1A is partially dominant in personata (heterozygote is 25% more similar to personata compared to addive model)
+# 20 is partially dominant in personata (heterozygote is 25% more similar to personata compared to addive model)
 
 pers.dom.1A_pers.dom20 <- lm(head.plumage.value ~ pers.dom.1A + pers.dom20 , data = data)
 
