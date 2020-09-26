0b17a33c216d3d251a033c024e45e1b543596d61
georgysemenov <71846417+georgysemenov@users.noreply.github.com>
2020-09-26 11:43:45 -0600

    Update Comparison of inheritance models.R

--- code/Comparison of inheritance models.R
+++ code/Comparison of inheritance models.R
@@ -1,9 +1,8 @@
 library(tidyverse)
 data <- read.table("compound.genotypes1A.20.txt", header=T)
-#this dataset includes only individuals sampled within alba and personata hybrid zone
-# head.plumage.value is a measure of the amount of white coloration on the head and neck plumage (see Semenov et al. 2017 Mol. Ecol. for plumage quantification details)
-# Input file contains genotypic information for two genomic regions on chromsome 1A (aa=alba allele, aP=heterozygote, PP=personata allele) and 20 (AA=alba allele, Ap=heterozygote,
-# pp=personata alleles) encoded as biallelic loci. Alleles with presumably partially dominant inheritance are capitilized.
+#this dataset includes only individuals sampled within the hyrbid zone
+
+############### ADDITIVE MODEL ################
 
 # Model with addiditive effects in both 1A and 20
 data$add1A <- gsub("PP", 1, data$genotype.1A)
@@ -17,6 +16,10 @@ data$add20 <- gsub("pp", 1, data$add20)
 data$add20 <- as.numeric(data$add20)
 
 additive <- lm(head.plumage.value ~ add1A + add20, data = data)
+#adding the epistatic term
+additive_epistasis <- lm(head.plumage.value ~ add1A + add20 + add1A:add20, data = data)
+
+############### PARTIAL DOMINANCE MODELS ################
 
 # Partially dominant model where:
 # 1A is partially dominant in personata (heterozygote is 25% more similar to personata compared to addive model) and
@@ -26,19 +29,21 @@ data$pers.dom.1A <- gsub("aP", 0.75, data$pers.dom.1A)
 data$pers.dom.1A <- gsub("aa", 0, data$pers.dom.1A)
 data$pers.dom.1A <- as.numeric(data$pers.dom.1A)
 
-data$alba.dom20 <- gsub("AA", 1, data$genotype.20)
-data$alba.dom20 <- gsub("Ap", 0.75, data$alba.dom20)
-data$alba.dom20 <- gsub("pp", 0, data$alba.dom20)
+data$alba.dom20 <- gsub("AA", 0, data$genotype.20)
+data$alba.dom20 <- gsub("Ap", 0.25, data$alba.dom20)
+data$alba.dom20 <- gsub("pp", 1, data$alba.dom20)
 data$alba.dom20 <- as.numeric(data$alba.dom20)
 
 pers.dom.1A_alba.dom20 <- lm(head.plumage.value ~ pers.dom.1A + alba.dom20, data = data)
+#adding the epistatic term
+pers.dom.1A_alba.dom20_epistasis <- lm(head.plumage.value ~ pers.dom.1A + alba.dom20 + pers.dom.1A:alba.dom20, data = data)
 
 # Partially dominant model where 
 # 1A is partially dominant in alba (heterozygote is 25% more similar to alba compared to addive model) and
 # 20 is partially dominant in personata (heterozygote is 25% more similar to personata compared to addive model)
-data$alba.dom1A <- gsub("PP", 0, data$genotype.1A)
-data$alba.dom1A <- gsub("aP", 0.75, data$alba.dom1A)
-data$alba.dom1A <- gsub("aa", 1, data$alba.dom1A)
+data$alba.dom1A <- gsub("PP", 1, data$genotype.1A)
+data$alba.dom1A <- gsub("aP", 0.25, data$alba.dom1A)
+data$alba.dom1A <- gsub("aa", 0, data$alba.dom1A)
 data$alba.dom1A <- as.numeric(data$alba.dom1A)
 
 data$pers.dom20 <- gsub("AA", 0, data$genotype.20)
@@ -48,11 +53,17 @@ data$pers.dom20 <- as.numeric(data$pers.dom20)
 
 alba.dom.1A_pers.dom20 <- lm(head.plumage.value ~ alba.dom1A + pers.dom20 , data = data)
 
+#adding the epistatic term
+alba.dom.1A_pers.dom20_epistasis <- lm(head.plumage.value ~ alba.dom1A + pers.dom20 + alba.dom1A:pers.dom20, data = data)
+
 # Partially dominant model where 
 # 1A is partially dominant in alba (heterozygote is 25% more similar to alba compared to addive model)
 # 20 is partially dominant in alba (heterozygote is 25% more similar to alba compared to addive model)
 
 alba.dom.1A_alba.dom20 <- lm(head.plumage.value ~ alba.dom1A + alba.dom20 , data = data)
+
+#adding the epistatic term
+alba.dom.1A_alba.dom20_epistasis <- lm(head.plumage.value ~ alba.dom1A + alba.dom20 + alba.dom1A:alba.dom20, data = data)
 
 # Partially dominant model where 
 # 1A is partially dominant in personata (heterozygote is 25% more similar to personata compared to addive model)
@@ -60,62 +71,124 @@ alba.dom.1A_alba.dom20 <- lm(head.plumage.value ~ alba.dom1A + alba.dom20 , data
 
 pers.dom.1A_pers.dom20 <- lm(head.plumage.value ~ pers.dom.1A + pers.dom20 , data = data)
 
-# model comparison. A model is considered significantly better if AIC (AICc, BIC) is lower by at least two points
-AIC(additive, pers.dom.1A_alba.dom20, alba.dom.1A_pers.dom20, alba.dom.1A_alba.dom20, pers.dom.1A_pers.dom20)
+#adding the epistatic term
+pers.dom.1A_pers.dom20_epistasis <- lm(head.plumage.value ~ pers.dom.1A + pers.dom20 + pers.dom.1A:pers.dom20, data = data)
 
-#                        df      AIC
-# additive                4 442.3477
-# pers.dom.1A_alba.dom20  4 427.3859 (Best model)
-# alba.dom.1A_pers.dom20  4 478.7637
-# alba.dom.1A_alba.dom20  4 441.5010
-# pers.dom.1A_pers.dom20  4 475.7174
+############### FULL DOMINANCE MODELS ################
 
-AICc(additive, pers.dom.1A_alba.dom20, alba.dom.1A_pers.dom20, alba.dom.1A_alba.dom20, pers.dom.1A_pers.dom20)
+# 1A is dominant in personata 
+# 20 is dominant in alba 
+data$pers.full.dom.1A <- gsub("PP", 1, data$genotype.1A)
+data$pers.full.dom.1A <- gsub("aP", 1, data$pers.full.dom.1A)
+data$pers.full.dom.1A <- gsub("aa", 0, data$pers.full.dom.1A)
+data$pers.full.dom.1A <- as.numeric(data$pers.full.dom.1A)
 
-#                        df     AICc
-# additive                4 443.0884
-# pers.dom.1A_alba.dom20  4 428.1267 (Best model)
-# alba.dom.1A_pers.dom20  4 479.5044
-# alba.dom.1A_alba.dom20  4 442.2417
-# pers.dom.1A_pers.dom20  4 476.4581
+data$alba.full.dom20 <- gsub("AA", 0, data$genotype.20)
+data$alba.full.dom20 <- gsub("Ap", 0, data$alba.full.dom20)
+data$alba.full.dom20 <- gsub("pp", 1, data$alba.full.dom20)
+data$alba.full.dom20 <- as.numeric(data$alba.full.dom20)
 
-BIC(additive, pers.dom.1A_alba.dom20, alba.dom.1A_pers.dom20, alba.dom.1A_alba.dom20, pers.dom.1A_pers.dom20)
+pers.full.dom.1A_alba.full.dom20 <- lm(head.plumage.value ~ pers.full.dom.1A + alba.full.dom20, data = data)
 
-#                        df      BIC
-# additive                4 450.6578
-# pers.dom.1A_alba.dom20  4 435.6961 (Best model)
-# alba.dom.1A_pers.dom20  4 487.0738
-# alba.dom.1A_alba.dom20  4 449.8111
-# pers.dom.1A_pers.dom20  4 484.0275
+#adding the epistatic term
+pers.full.dom.1A_alba.full.dom20_epistasis <- lm(head.plumage.value ~ pers.full.dom.1A + alba.full.dom20 + pers.full.dom.1A:alba.full.dom20, data = data)
+ 
+# 1A is dominant in alba 
+# 20 is dominant in personata 
+data$alba.full.dom1A <- gsub("PP", 1, data$genotype.1A)
+data$alba.full.dom1A <- gsub("aP", 0, data$alba.full.dom1A)
+data$alba.full.dom1A <- gsub("aa", 0, data$alba.full.dom1A)
+data$alba.full.dom1A <- as.numeric(data$alba.full.dom1A)
 
-# pers.dom.1A_alba.dom20 (hypothetical inheritance mechnaism) is the best model across all models tested.
+data$pers.full.dom20 <- gsub("AA", 0, data$genotype.20)
+data$pers.full.dom20 <- gsub("Ap", 1, data$pers.full.dom20)
+data$pers.full.dom20 <- gsub("pp", 1, data$pers.full.dom20)
+data$pers.full.dom20 <- as.numeric(data$pers.full.dom20)
 
-# Now adding the epistasis term to the best model from above.
-pers.dom.1A_alba.dom20_epistasis <- lm(head.plumage.value ~ pers.dom.1A + alba.dom20 + pers.dom.1A:alba.dom20, data = data)
+alba.full.dom.1A_pers.full.dom20 <- lm(head.plumage.value ~ alba.full.dom1A + pers.full.dom20 , data = data)
 
-AIC(additive, pers.dom.1A_alba.dom20, alba.dom.1A_pers.dom20, alba.dom.1A_alba.dom20, pers.dom.1A_pers.dom20, pers.dom.1A_alba.dom20_epistasis)
+#adding the epistatic term
+alba.full.dom.1A_pers.full.dom20_epistasis <- lm(head.plumage.value ~ alba.full.dom1A + pers.full.dom20 + alba.full.dom1A:pers.full.dom20, data = data)
 
-#                                  df      AIC
-# additive                          4 442.3477
-# pers.dom.1A_alba.dom20            4 427.3859
-# alba.dom.1A_pers.dom20            4 478.7637
-# alba.dom.1A_alba.dom20            4 441.5010
-# pers.dom.1A_pers.dom20            4 475.7174
-# pers.dom.1A_alba.dom20_epistasis  5 423.3109 (Best model)
 
-#Comparing the addiditve + epistatsis model to pers.dom.1A_alba.dom20_epistasis.
-additive_epistasis <- lm(head.plumage.value ~ add1A + add20 + add1A:add20, data = data)
+# 1A is dominant in alba 
+# 20 is dominant in alba 
 
-AIC(additive, pers.dom.1A_alba.dom20, alba.dom.1A_pers.dom20, alba.dom.1A_alba.dom20, pers.dom.1A_pers.dom20, pers.dom.1A_alba.dom20_epistasis, additive_epistasis)
+alba.full.dom.1A_alba.full.dom20 <- lm(head.plumage.value ~ alba.full.dom1A + alba.full.dom20 , data = data)
 
-#                                  df      AIC
-# additive                          4 442.3477
-# pers.dom.1A_alba.dom20            4 427.3859
-# alba.dom.1A_pers.dom20            4 478.7637
-# alba.dom.1A_alba.dom20            4 441.5010
-# pers.dom.1A_pers.dom20            4 475.7174
-# pers.dom.1A_alba.dom20_epistasis  5 423.3109 (Best model)
-# additive_epistasis                5 444.3477
+#adding the epistatic term
+alba.full.dom.1A_alba.full.dom20_epsitasis <- lm(head.plumage.value ~ alba.full.dom1A + alba.full.dom20 + alba.full.dom1A:alba.full.dom20, data = data)
 
-# Model with partial dominance of personata alleles on chr 1A and partial dominance of alba alleles on chr 20, and with epistatic interaction between the two regions
-# is the best in describing the inheritance mechanism of head plumage. Different measures of model comparison (AIC, BIC and AICc) produce congruent results.
+
+# 1A is dominant in personata
+# 20 is dominant in personata 
+pers.full.dom.1A_pers.full.dom20 <- lm(head.plumage.value ~ pers.full.dom.1A + pers.full.dom20 , data = data)
+
+#adding the epistatic term
+pers.full.dom.1A_pers.full.dom20_epistasis <- lm(head.plumage.value ~ pers.full.dom.1A + pers.full.dom20 + pers.full.dom.1A:pers.full.dom20, data = data)
+
+
+############### MODEL COMPARISON ################
+
+AIC(additive, 
+    additive_epistasis,
+    pers.dom.1A_alba.dom20,
+    pers.dom.1A_alba.dom20_epistasis,
+    alba.dom.1A_pers.dom20,
+    alba.dom.1A_pers.dom20_epistasis,
+    alba.dom.1A_alba.dom20,
+    alba.dom.1A_alba.dom20_epistasis,
+    pers.dom.1A_pers.dom20,
+    pers.dom.1A_pers.dom20_epistasis,
+    pers.full.dom.1A_alba.full.dom20,
+    pers.full.dom.1A_alba.full.dom20_epistasis,
+    alba.full.dom.1A_pers.full.dom20,
+    alba.full.dom.1A_pers.full.dom20_epistasis,
+    alba.full.dom.1A_alba.full.dom20,
+    alba.full.dom.1A_alba.full.dom20_epsitasis,
+    pers.full.dom.1A_pers.full.dom20,
+    pers.full.dom.1A_pers.full.dom20_epistasis
+    )
+
+AICc(additive, 
+    additive_epistasis,
+    pers.dom.1A_alba.dom20,
+    pers.dom.1A_alba.dom20_epistasis,
+    alba.dom.1A_pers.dom20,
+    alba.dom.1A_pers.dom20_epistasis,
+    alba.dom.1A_alba.dom20,
+    alba.dom.1A_alba.dom20_epistasis,
+    pers.dom.1A_pers.dom20,
+    pers.dom.1A_pers.dom20_epistasis,
+    pers.full.dom.1A_alba.full.dom20,
+    pers.full.dom.1A_alba.full.dom20_epistasis,
+    alba.full.dom.1A_pers.full.dom20,
+    alba.full.dom.1A_pers.full.dom20_epistasis,
+    alba.full.dom.1A_alba.full.dom20,
+    alba.full.dom.1A_alba.full.dom20_epsitasis,
+    pers.full.dom.1A_pers.full.dom20,
+    pers.full.dom.1A_pers.full.dom20_epistasis
+)
+
+BIC(additive, 
+    additive_epistasis,
+    pers.dom.1A_alba.dom20,
+    pers.dom.1A_alba.dom20_epistasis,
+    alba.dom.1A_pers.dom20,
+    alba.dom.1A_pers.dom20_epistasis,
+    alba.dom.1A_alba.dom20,
+    alba.dom.1A_alba.dom20_epistasis,
+    pers.dom.1A_pers.dom20,
+    pers.dom.1A_pers.dom20_epistasis,
+    pers.full.dom.1A_alba.full.dom20,
+    pers.full.dom.1A_alba.full.dom20_epistasis,
+    alba.full.dom.1A_pers.full.dom20,
+    alba.full.dom.1A_pers.full.dom20_epistasis,
+    alba.full.dom.1A_alba.full.dom20,
+    alba.full.dom.1A_alba.full.dom20_epsitasis,
+    pers.full.dom.1A_pers.full.dom20,
+    pers.full.dom.1A_pers.full.dom20_epistasis
+)    
+
+# Summary of the best model  
+summary(pers.dom.1A_alba.dom20_epistasis)
